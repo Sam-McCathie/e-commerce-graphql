d7b875451dd133a70fca702fe5d110775a0ab15b
Sam <sammccathie2@gmail.com>
2022-04-18 14:46:03 +1200

    Added  basic Scalar types and installed nodemon

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const {ApolloServer, gql} = require("apollo-server");
 
+// Scaler types
+// - String
+// - Int
+// - Float
+// - Boolean
+// - ID
+
 // Type definitions of data/ how it is going to look
+// adds type safety
+// ! - prevents null values from being returned
 const typeDefs = gql`
   type Query {
-    hello: String
+    aString: String!
+    anInt: Int
+    aFloat: Float
+    aBoolean: Boolean
   }
 `;
 
 // Resolvers return the data
+// resolver names must match the type definitions above
 const resolvers = {
   Query: {
-    hello: () => {
+    aString: () => {
       return "World!@";
+    },
+    anInt: () => {
+      return 2022;
+    },
+    aFloat: () => {
+      return 1.5;
+    },
+    aBoolean: () => {
+      return true;
     },
   },
 };
